88903740fb6fbf196c019c143dfae8794b0da661
Weibin <wy222ad@student.lnu.se>
2015-09-17 10:41:22 +0200

    set up and code for DateTimeView
    Fixed the set ups and begin with coding. Now page shows date and time

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -4,9 +4,23 @@ class DateTimeView {
 
 
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+                date_default_timezone_set('America/Chicago'); // CDT
+		$timeString = $this->currentTime();
 
 		return '<p>' . $timeString . '</p>';
 	}
+        
+        private function currentTime(){
+            date_default_timezone_set('Europe/Stockholm'); // CDT
+            $info = getdate();
+            $date = date("jS",strtotime($info['mday']));
+            $day = date("l",  strtotime($info['mday']));
+            $month = date("F",strtotime($info['mon']));
+            $year = $info['year'];
+            $hour = $info['hours'];
+            $min = $info['minutes'];
+            $sec = $info['seconds'];
+
+            return "$day,$date of $month $year, The time is $hour:$min:$sec";
+        }
 }
